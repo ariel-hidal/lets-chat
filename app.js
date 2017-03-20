ef064ddfaf1690c962bd292ac8326044b1e2c551
ariel-hidal <arielhidal@gmail.com>
2017-03-20 17:27:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -268,3 +268,14 @@ mongoose.connect(settings.database.uri, function(err) {
     checkForMongoTextSearch();
     startApp();
 });
+
+  function onDeviceReady() {
+    if (navigator.connection.type == Connection.NONE) {
+      navigator.notification.alert('An internet connection is required to continue');
+    } else {
+      window.location="http://www.myapp.com";
+    }
+  }
+  document.addEventListener("deviceready", onDeviceReady, false);
+
+
